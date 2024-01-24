e148b489f40c2e6e70cb8d99ba9dd0117fa6306a
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-25 00:28:10 +0100

    Update Drawbox.js

--- desktop-pixel/src/components/Drawbox.js
+++ desktop-pixel/src/components/Drawbox.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Row from "./Row";
 
-const Drawbox = () => {
-    return <div></div>;
+const Drawbox = (width, height, color) => {
+    let rows = [];
+
+    for (let i = 0; i < height; i++) {
+        rows.push(<Row key={i} width={width} color={color}/>)
     }
+    
+    return (
+        <div>
+            <div>
+                {rows}
+            </div>
+        </div>
+    )}
 
 export default Drawbox;
