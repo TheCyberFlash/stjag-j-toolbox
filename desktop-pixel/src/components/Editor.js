3ee39d5b42e7c4b35824c9b1402e01f35eb69e2f
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-25 00:18:41 +0100

    Update Editor.js

--- desktop-pixel/src/components/Editor.js
+++ desktop-pixel/src/components/Editor.js
@@ -1,23 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Editor = () => {
+    const [height, setHeight] = useState(16);
+    const [width, setWidth] = useState(16);
+    const [hideOptions, setHideOptions] = useState(false);
+    const [hideDrawbox, setHideDrawbox] = useState(true);
+    const [buttonText, setButtonText] = useState("Start Drawbox");
+
+    const handleButtonClick = () => {
+        setHideOptions(!hideOptions);
+        setHideDrawbox(!hideDrawbox);
+        setButtonText(buttonText === "Start Drawbox" ? "Reset Drawbox" : "Start Drawbox");
+    }
     return (
         <div>
-            <h1>Editor</h1>
-            <h2>Choose Panel Dimensions</h2>
+            <h1>Editor</h1>            
 
+            {hideDrawbox && 
             <div>
+                <h2>Choose Panel Dimensions</h2>
                 <div>
-                    <input type="number" defaultValue="16" />
+                    <input type="number" defaultValue={height} onChange={(e) => {setHeight(e.target.value)}}/>
                     <span>Height</span>
                 </div>
                 <div>
-                    <input type="number" defaultValue="16" />
+                    <input type="number" defaultValue={width} onChange={(e) => {setWidth(e.target.value)}}/>
                     <span>Width</span>
                 </div>
             </div>
+            }
 
-            <button>Start Drawbox</button>
+            <button onClick={handleButtonClick}>{buttonText}</button>
         </div>
     )}
 
