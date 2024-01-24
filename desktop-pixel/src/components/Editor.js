f06d7188fbef17550aa78fb482d3711515cc9bf6
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-25 00:12:42 +0100

    Update Editor.js

--- desktop-pixel/src/components/Editor.js
+++ desktop-pixel/src/components/Editor.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Editor = () => {
-    return <div></div>;
-    }
+    return (
+        <div>
+            <h1>Editor</h1>
+            <h2>Choose Panel Dimensions</h2>
+
+            <div>
+                <div>
+                    <input type="number" defaultValue="16" />
+                    <span>Height</span>
+                </div>
+                <div>
+                    <input type="number" defaultValue="16" />
+                    <span>Width</span>
+                </div>
+            </div>
+
+            <button>Start Drawbox</button>
+        </div>
+    )}
 
 export default Editor;
