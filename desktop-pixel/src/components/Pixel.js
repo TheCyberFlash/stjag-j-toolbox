8ebf15068501119932bd0a77ae89d90b3c7d6418
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-25 00:35:09 +0100

    Update Pixel.js

--- desktop-pixel/src/components/Pixel.js
+++ desktop-pixel/src/components/Pixel.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Pixel = () => {
-    return <div></div>;
+const Pixel = (color) => {
+
+    const [pixelColor, setPixelColor] = useState(color);
+    const [prevColor, setPrevColor] = useState(pixelColor);
+    const [canChange, setCanChange] = useState(true);
+
+    const applyColor = () => {
+            setPixelColor(color);
+            setCanChange(false);        
     }
+
+    const resetColor = () => {
+        if (canChange) {
+            setPixelColor(prevColor);
+        }
+
+        setCanChange(true);
+    }
+
+    const handleMouseEnter = () => {
+        setPrevColor(pixelColor);
+        setPixelColor(color);
+    }
+    return (
+        <div style={{ backgroundColor: pixelColor}} onClick={applyColor} onMouseEnter={handleMouseEnter} onMouseLeave={resetColor}>
+
+        </div>
+    )}
 
 export default Pixel;
