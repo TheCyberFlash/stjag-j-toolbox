ee4ef0176356792c8f42833410aa94327706a869
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-26 18:57:49 +0100

    Update Toolbar.js

--- desktop-pixel/src/components/Toolbar.js
+++ desktop-pixel/src/components/Toolbar.js
@@ -1,15 +1,36 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faExpand, // - Resize Canvas
+    faPalette, // - Change Color
+    faUndo, // - Undo
+    faRedo, // - Redo
+    faTrash, // - Clear Canvas
+    faImage // - Export as PNG
+} from "@fortawesome/free-solid-svg-icons";
 
 const Toolbar = () => {
 
     return (
         <div className="toolbar">
-            <button className="toolbar-button">Resize Canvas</button>
-            <button className="toolbar-button">Change Color</button>
-            <button className="toolbar-button">Undo</button>
-            <button className="toolbar-button">Redo</button>
-            <button className="toolbar-button">Clear Canvas</button>
-            <button className="toolbar-button">Export as PNG</button>
+            <button className="toolbar-button" id="resize-canvas">
+                <FontAwesomeIcon icon={faExpand} />
+            </button>
+            <button className="toolbar-button" id="change-color">
+                <FontAwesomeIcon icon={faPalette} />
+            </button>
+            <button className="toolbar-button" id="undo">
+                <FontAwesomeIcon icon={faUndo} />
+            </button>
+            <button className="toolbar-button" id="redo">
+                <FontAwesomeIcon icon={faRedo} />
+            </button>
+            <button className="toolbar-button" id="clear-canvas">
+                <FontAwesomeIcon icon={faTrash} />
+            </button>
+            <button className="toolbar-button" id="export-png">
+                <FontAwesomeIcon icon={faImage} />
+            </button>
         </div>
     )
 }
