1a7a2b2c51719a249d20e92638c8e5a18f829213
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-26 19:19:56 +0100

    Update PixelArtCanvas.js

--- desktop-pixel/src/components/PixelArtCanvas.js
+++ desktop-pixel/src/components/PixelArtCanvas.js
@@ -5,6 +5,8 @@ import Toolbar from "./Toolbar";
 const PixelArtCanvas = ({ height, width, color }) => {
     const canvasRef = useRef(null);
     const [drawing, setDrawing] = useState(false);
+    const [canvasStateStack, setCanvasStateStack] = useState([]);
+    const [currentCanvasState, setCurrentCanvasState] = useState(null);
 
     useEffect(() => {
         const canvas = canvasRef.current;
@@ -12,7 +14,51 @@ const PixelArtCanvas = ({ height, width, color }) => {
 
         context.fillStyle = "white";
         context.fillRect(0, 0, canvas.width, canvas.height);
+        saveCanvasState();
     }, [height, width]);
+
+    const saveCanvasState = () => {
+        const canvas = canvasRef.current;
+        const snapshot = canvas.toDataURL("image/png");
+        setCanvasStateStack((prevStack) => [...prevStack, snapshot]);
+        setCurrentCanvasState(snapshot);
+    };
+
+    const restoreCanvasState = (state) => {
+        const canvas = canvasRef.current;
+        const context = canvas.getContext("2d");
+        const img = new Image();
+
+        img.src = state;
+        img.onload = () => {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.drawImage(img, 0, 0);
+        };
+    };
+
+    const handleUndo = () => {
+        if (canvasStateStack.length > 1) {
+            const prevState = canvasStateStack.slice(0, -1);
+            setCanvasStateStack(prevState);
+            restoreCanvasState(prevState[prevState.length - 1]);
+        }
+    };
+
+    const handleRedo = () => {
+        if (currentCanvasState && canvasStateStack.length > 1) {
+            const nextState = canvasStateStack.slice(1);
+            setCanvasStateStack([currentCanvasState, ...nextState]);
+            restoreCanvasState(nextState[0]);
+        }
+    };
+
+    const handleReset = () => {
+        if (currentCanvasState && canvasStateStack.length > 1) {
+            const nextState = canvasStateStack.slice(1);
+            setCanvasStateStack([currentCanvasState, ...nextState]);
+            restoreCanvasState(nextState[0]);
+        }
+    };
 
     const startDrawing = (event) => {
         setDrawing(true);
@@ -44,15 +90,15 @@ const PixelArtCanvas = ({ height, width, color }) => {
 
     const handleTouchStart = (event) => {
         startDrawing(event.touches[0]);
-      };
+    };
     
-      const handleTouchMove = (event) => {
+    const handleTouchMove = (event) => {
         draw(event.touches[0]);
-      };
+    };
     
-      const handleTouchEnd = () => {
+    const handleTouchEnd = () => {
         endDrawing();
-      };
+    };
 
     const handleSaveImage = () => {
 
@@ -60,12 +106,16 @@ const PixelArtCanvas = ({ height, width, color }) => {
         const fileName = `${today}JsTExport.png`;
       
         exportComponentAsPNG(canvasRef, { fileName });
-      };
-      
+    };
 
     return (
         <div>
-            <Toolbar/>
+            <Toolbar
+                onUndo={handleUndo}
+                onRedo={handleRedo}
+                onReset={handleReset}
+                onExport={handleSaveImage}
+            />
 
             <div>
                 <canvas
