882049197214210472d66e75d18ed81f7756a7af
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-27 11:48:00 +0100

    Update Editor.js

--- desktop-pixel/src/components/Editor.js
+++ desktop-pixel/src/components/Editor.js
@@ -1,27 +1,14 @@
 import React, {useState} from "react";
-import { CirclePicker, PhotoshopPicker, SketchPicker } from "react-color";
 import PixelArtCanvas from "./PixelArtCanvas";
 
 const Editor = () => {
     const [height, setHeight] = useState(16);
     const [width, setWidth] = useState(16);
-    const [color, setColor] = useState("#fff");
-    const [colorSelect, setColorSelect] = useState(false);
-
-    const handleColorChange = (color) => {
-        setColor(color.hex);
-    }
 
     return (
         <div id="editor">
             <h1>Js Toolbox - Pixel Canvas</h1>                
-                <PixelArtCanvas height={height} width={width} color={color} colorSelect={colorSelect} setColorSelect={setColorSelect}/>
-
-                {colorSelect && (
-                <CirclePicker color={color} onChangeComplete={handleColorChange}/>
-                // <PhotoshopPicker color={color} onChangeComplete={handleColorChange}/>
-                // <SketchPicker color={color} onChangeComplete={handleColorChange}/>
-                )}            
+                <PixelArtCanvas height={height} width={width}/>          
             </div>
     )}
 
