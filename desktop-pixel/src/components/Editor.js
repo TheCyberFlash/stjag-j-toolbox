f38b0b568072e776a033521a04b6f9a60c9c823d
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-27 11:52:33 +0100

    Update Editor.js

--- desktop-pixel/src/components/Editor.js
+++ desktop-pixel/src/components/Editor.js
@@ -1,15 +1,16 @@
-import React, {useState} from "react";
-import PixelArtCanvas from "./PixelArtCanvas";
+import React, { useState } from 'react';
+import PixelArtCanvas from './PixelArtCanvas';
 
 const Editor = () => {
-    const [height, setHeight] = useState(16);
-    const [width, setWidth] = useState(16);
+  const [height, setHeight] = useState(16);
+  const [width, setWidth] = useState(16);
 
-    return (
-        <div id="editor">
-            <h1>Js Toolbox - Pixel Canvas</h1>                
-                <PixelArtCanvas height={height} width={width}/>          
-            </div>
-    )}
+  return (
+    <div id="editor">
+      <h1>Js Toolbox - Pixel Canvas</h1>
+      <PixelArtCanvas height={height} width={width} />
+    </div>
+  );
+};
 
 export default Editor;
