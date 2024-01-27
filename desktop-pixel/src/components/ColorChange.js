d75456184877b7faad5bafb3455fd2e34482594c
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-27 11:52:11 +0100

    Update ColorChange.js

--- desktop-pixel/src/components/ColorChange.js
+++ desktop-pixel/src/components/ColorChange.js
@@ -1,16 +1,21 @@
-import React from "react";
-import { CirclePicker, PhotoshopPicker, SketchPicker } from "react-color";
+import React from 'react';
+import { CirclePicker, PhotoshopPicker, SketchPicker } from 'react-color';
 
-const ColorChange = ({ color, colorSelect, setColorSelect, handleColorChange }) => {
-    return (
-        <div className="color-select">
-            {colorSelect && (
-                // <CirclePicker color={color} onChangeComplete={handleColorChange}/>
-                // <PhotoshopPicker color={color} onChangeComplete={handleColorChange}/>
-                <SketchPicker color={color} onChangeComplete={handleColorChange}/>
-            )}
-        </div>
-    )
-}
+const ColorChange = ({
+  color,
+  colorSelect,
+  setColorSelect,
+  handleColorChange,
+}) => {
+  return (
+    <div className="color-select">
+      {colorSelect && (
+        // <CirclePicker color={color} onChangeComplete={handleColorChange}/>
+        // <PhotoshopPicker color={color} onChangeComplete={handleColorChange}/>
+        <SketchPicker color={color} onChangeComplete={handleColorChange} />
+      )}
+    </div>
+  );
+};
 
 export default ColorChange;
