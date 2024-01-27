03682c3c011a3e2f73330542ca5a81b235a0b393
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-27 11:52:16 +0100

    Update ResizeCanvas.js

--- desktop-pixel/src/components/ResizeCanvas.js
+++ desktop-pixel/src/components/ResizeCanvas.js
@@ -1,41 +1,45 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const ResizeCanvas = ({ onCancel, onSubmit }) => {
-    const [newWidth, setNewWidth] = useState("");
-    const [newHeight, setNewHeight] = useState("");
+  const [newWidth, setNewWidth] = useState('');
+  const [newHeight, setNewHeight] = useState('');
 
-    const handleResizeSubmit = (e) => {
-        e.preventDefault();
-        onSubmit(newWidth, newHeight);
-    };
+  const handleResizeSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(newWidth, newHeight);
+  };
 
-    const handleResizeCancel = () => {
-        onCancel();
-    };
+  const handleResizeCancel = () => {
+    onCancel();
+  };
 
-    return (
-        <div className="resize-canvas-container">
-            <label>
-                W
-                <input 
-                    type="number" 
-                    value={newWidth}
-                    onChange={(event) => setNewWidth(event.target.value)}
-                />
-            </label>
+  return (
+    <div className="resize-canvas-container">
+      <label>
+        W
+        <input
+          type="number"
+          value={newWidth}
+          onChange={(event) => setNewWidth(event.target.value)}
+        />
+      </label>
 
-            <label>
-                H
-                <input 
-                    type="number" 
-                    value={newHeight}
-                    onChange={(event) => setNewHeight(event.target.value)}
-                />
-            </label>
-            <button onClick={handleResizeSubmit} className="emoji-button">✅</button>
-            <button onClick={handleResizeCancel} className="emoji-button">❌</button>    
-        </div>
-    )
+      <label>
+        H
+        <input
+          type="number"
+          value={newHeight}
+          onChange={(event) => setNewHeight(event.target.value)}
+        />
+      </label>
+      <button onClick={handleResizeSubmit} className="emoji-button">
+        ✅
+      </button>
+      <button onClick={handleResizeCancel} className="emoji-button">
+        ❌
+      </button>
+    </div>
+  );
 };
 
 export default ResizeCanvas;
