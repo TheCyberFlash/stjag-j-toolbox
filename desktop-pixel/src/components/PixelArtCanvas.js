591af2662173c4298da0b9edbb02f5eef3af3f1c
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-28 16:51:24 +0100

    Update PixelArtCanvas.js

--- desktop-pixel/src/components/PixelArtCanvas.js
+++ desktop-pixel/src/components/PixelArtCanvas.js
@@ -30,24 +30,27 @@ const PixelArtCanvas = () => {
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d');
     const cellSize = canvas.width / width;
-
+  
     const newState = Array.from({ length: height }, () =>
       Array.from({ length: width }, () => 'rgba(0, 0, 0, 0)'),
     );
-
+  
     for (let row = 0; row < height; row++) {
       for (let col = 0; col < width; col++) {
         const x = col * cellSize;
         const y = row * cellSize;
         const imageData = context.getImageData(x, y, cellSize, cellSize).data;
-
+  
         const color = `rgba(${imageData[0]}, ${imageData[1]}, ${imageData[2]}, ${imageData[3]})`;
         newState[row][col] = color;
       }
     }
-
-    setCanvasStateStack((prevStack) => [...prevStack.slice(-9), newState]);
-    setCurrentStateIndex((prevIndex) => Math.min(prevIndex + 1, 9));
+  
+    setCanvasStateStack((prevStack) => {
+      const newStack = [...prevStack.slice(-9), newState];
+      setCurrentStateIndex((prevIndex) => Math.min(prevIndex + 1, 9));
+      return newStack;
+    });
   };
 
   const restoreCanvasState = (state) => {
